1e41953cad3fd47a3774f35e778014f03a809b37
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-25 02:41:57 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,10 +2,10 @@ require 'pry'
 class Helpers
   def self.current_user(session)
     User.find_by(id: session[:user_id])
-    binding.pry
   end
 
   def self.is_logged_in?(session)
     !!User.find_by(id: session[:user_id])
   end
+    binding.pry
 end
