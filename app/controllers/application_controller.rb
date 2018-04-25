3b3b0dde89a242b01d7670f3c007f83978e354bf
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-25 01:46:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params["username"])
     if !!@user
+      session[:user_id] = @user.id
       redirect to '/account'
     else
       erb :error
@@ -22,7 +23,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    session[:user_id] = @user.id
     erb :account
   end
 
